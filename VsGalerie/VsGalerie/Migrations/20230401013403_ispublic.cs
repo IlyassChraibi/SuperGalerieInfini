d867093d4bad48cfc6935894d5bc2b2ce5032958
ilyas <1925619@cegepmontpetit.ca>
2023-04-02 18:40:36 -0400

    FCT Ajouter une galierie

---
+++ VsGalerie/VsGalerie/Migrations/20230401013403_ispublic.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VsGalerie.Migrations
+{
+    public partial class ispublic : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Public",
+                table: "Galerie",
+                newName: "IsPublic");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsPublic",
+                table: "Galerie",
+                newName: "Public");
+        }
+    }
+}
